9b3007ebcba74923677d4fdc7142cdece53a147f
hyeyeong <kkotburiwillbloom@gmail.com>
2023-06-07 18:07:04 +0900

    video api implementation

--- js/detail.js
+++ js/detail.js
@@ -1,3 +1,5 @@
 import { detailInfo } from "./modules/detail-info.js";
+import { fetchMovieVideo } from "./modules/detail-video-hy.js";
 
 detailInfo();
+fetchMovieVideo();
--- js/modules/detail-info.js
+++ js/modules/detail-info.js
@@ -1,10 +1,10 @@
-export const detailInfo = () => {
-  const key = "4e657bab9a1d4d7b73eb2631af49f6da";
-  const movieDetailsElement = document.getElementById("movieDetails");
+export const urlParams = new URLSearchParams(window.location.search);
+export const movieId = urlParams.get("id");
 
-  const urlParams = new URLSearchParams(window.location.search);
-  console.log(window.location.search, urlParams);
-  const movieId = urlParams.get("id");
+const key = "4e657bab9a1d4d7b73eb2631af49f6da";
+const movieDetailsElement = document.getElementById("movieDetails");
+
+export const detailInfo = () => {
   async function fetchMovieDetails(movieId) {
     const url = `https://api.themoviedb.org/3/movie/${movieId}?api_key=${key}&append_to_response=credits`;
     const res = await fetch(url);
