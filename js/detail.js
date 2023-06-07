d9ee258f761feb0deedc2a960a55138cf5a12261
hyeyeong <kkotburiwillbloom@gmail.com>
2023-06-08 06:22:15 +0900

    trailer videos implementation

--- js/detail.js
+++ js/detail.js
@@ -1,5 +1,5 @@
 import { detailInfo } from "./modules/detail-info.js";
-import { fetchMovieVideo } from "./modules/detail-video-hy.js";
+import { fetchMovieVideo } from "./modules/fetchMovieVideo-hy.js";
 
 detailInfo();
 fetchMovieVideo();
